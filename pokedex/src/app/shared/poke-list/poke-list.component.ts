55e01aa991340af768f5ba1bf11e3291ad7b4ad7
Lesley A Diniz <lesleydiniz@gmail.com>
2023-06-09 10:50:08 -0300

    Finishing Pokedex project
    Finishing Pokedex project

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { PokeApiService } from 'src/app/service/poke-api.service';
+
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeListComponent implements OnInit {
 
-  constructor() { }
+  private setAllPokemons: any;
+  public getAllPokemons: any;
+
+  public apiError: boolean = false;
+
+  constructor(
+    private pokeApiService: PokeApiService
+    ) { }
 
   ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      },
+      error => {
+        this.apiError = true;
+      }
+    );
   }
 
+  public getSearch(value: string): void {
+    const filter = this.setAllPokemons.filter(
+      (res: any) => {
+        return !res.name.indexOf(value.toLowerCase())
+      }
+    );
+
+    this.getAllPokemons = filter;
+  }
 }
